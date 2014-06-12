028a0ff0a2b31bef3169f620c8249ac604610252
yamada-shinji <jigyakkuma@gmail.com>
2014-06-12 14:54:19 +0900

    add

---
+++ plugin_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+[ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+[ ! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
+
