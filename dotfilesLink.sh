f4147ca96bfd27b279e335952a43110a91cbeb61
jigyakkuma <jigyakkuma@gmail.com>
2016-04-05 11:16:01 +0900

    Update provisioning

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .zshrc .vimrc .gitconfig .bashrc .peco/config.json .byobu/status)
+DOT_FILES=( .zshrc .vimrc .dein.toml .dein_lazy.toml .gitconfig .bashrc .peco/config.json .byobu/status)
  
 for file in ${DOT_FILES[@]}
 do
--- plugin_install.sh
+++ plugin_install.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
-[ ! -d ~/.vim/neobundle.vim ] && git clone git://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
 
